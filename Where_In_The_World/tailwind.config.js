6816dca378a43bb6d9a42679b68293b6c2e99eaa
Bryan Malunje <62018768+GISSIAH@users.noreply.github.com>
2023-02-27 18:45:13 +0200

    Feat: added tailwind, removed bootstrap

---
+++ Where_In_The_World/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["**/*.razor", "**/*.cshtml", "**/*.html"], // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
