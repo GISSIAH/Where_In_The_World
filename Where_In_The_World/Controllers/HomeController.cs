fe665ad8ec05600f59669fdbdcd5e50fba1afb55
Bryan Malunje <62018768+GISSIAH@users.noreply.github.com>
2023-02-27 18:20:24 +0200

    Add project files.

---
+++ Where_In_The_World/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Where_In_The_World.Models;
+
+namespace Where_In_The_World.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Where_In_The_World/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Where_In_The_World.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
